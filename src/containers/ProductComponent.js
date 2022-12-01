1b9010a59d4f39745aebe8dfea57b4a3f7b0a0f2
shubhambhawsar6 <98647679+shubhambhawsar6@users.noreply.github.com>
2022-12-01 18:10:40 +0530

    Updated

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -4,38 +4,33 @@ import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
   const products = useSelector((state) => state.allProducts.products);
-  const renderList = products.map((product) => {
+  const renderList = Object.values(products).map((product) => {
     const { id , title,  price, discountPercentage, rating, stock, brand,  category} = product;
     return (
       <div className="four wide column" key={id}>
         <Link to={`/product/${id}`}>
           <div>
-          <table>
-          <thead>
-            <tr>
-              
-              <th>Product Name</th>
-              <th>price</th>
-              <th>Discounted percentage</th>
-              <th>rating</th>
-              <th>stock</th>
-              <th>brand</th>
-              <th>category</th>
-              
-            </tr>
-          </thead>
-            <tbody>
-              <tr onClick={this.handleClick.bind(this)}>
-                <td>{title}</td>
-                <td>$ {price}</td>
-                <td>{discountPercentage}</td>
-                <td>{rating}</td>
-                <td>{stock}</td>
-                <td>{brand}</td>
-                <td>{category}</td>
-              </tr>
-            </tbody>
-        </table>
+            <div className="meta">
+              <h4>Title{title}</h4>
+            </div>
+            <div className="meta">
+              <h4>Price</h4>{price}
+            </div>
+            <div className="meta">
+              <h4>discountPercentage</h4>{discountPercentage}
+            </div>
+            <div className="meta">
+              <h4>rating</h4>{rating}
+            </div>
+            <div className="meta">
+              <h4>stock</h4>{stock}
+            </div>
+            <div className="meta">
+              <h4>brand</h4>{brand}
+            </div> 
+            <div className="meta">
+              <h4>category</h4>{category}
+            </div>
           </div>
         </Link>
       </div>
